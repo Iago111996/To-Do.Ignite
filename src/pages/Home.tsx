2f8cb3ac70dd7fa4d6adb5b249398b6c555b0055
Iago Pinto <iago111996@gmail.com>
2021-09-16 09:04:19 -0300

    implementado Alerts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Alert } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
@@ -10,12 +10,23 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    const updatedTasks = tasks.map(task => ({ ...task }));
+
+    const foundItem = updatedTasks.find(item => item.title === newTaskTitle);
+    if(foundItem) {
+      return Alert.alert(
+        'Task já cadastrada',
+        'Você não pode cadastrar uma task com o mesmo nome'
+      );
+    }
+
     const data = {
       id: new Date().getTime(),
       title: newTaskTitle,
       done: false,
     }
     setTasks(oldState => [...oldState, data]);
+
   }
 
   //como alterar um a chave de um object sem ferir a imutabilidade
@@ -32,9 +43,19 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    setTasks(oldState => oldState.filter(task => 
-      task.id != id
-     ));
+    Alert.alert(
+      'Remover item',
+      'Tem certeza que você deseja remover esse item?',
+      [
+        {
+          text: "Nâo",
+        },
+        {
+          text: 'Sim',
+          onPress: () => setTasks(oldState => oldState.filter(task => task.id != id))
+        }
+      ]
+    );
   }
 
   return (
